f62e5115290b8c21dce19d78a65d3c7c20f2a4bf
Alexander XX <freecoder.xx@gmail.com>
2020-06-13 15:22:34 +0300

    Add header and italic tools

--- src/simple/toolbar.rs
+++ src/simple/toolbar.rs
@@ -15,7 +15,11 @@ impl Default for SimpleToolbar {
         Self {
             id: "".to_string(),
             class: "lew-simple__toolbar".to_string(),
-            tools: vec![Box::new(tool::Bold::new())],
+            tools: vec![
+                Box::new(tool::Header::new()),
+                Box::new(tool::Bold::new()),
+                Box::new(tool::Italic::new()),
+            ],
         }
     }
 }
@@ -131,6 +135,47 @@ pub mod tool {
     use super::replace_selected_in_textarea;
     use crate::Widget;
 
+    pub struct Header {
+        pub textarea_selector: String,
+        pub class: String,
+        pub title: String,
+        pub size: u32,
+    }
+
+    impl Default for Header {
+        fn default() -> Self {
+            Self {
+                textarea_selector: ".lew-simple__textarea".to_string(),
+                class: "lew-simple__tool_button".to_string(),
+                title: "Header".to_string(),
+                size: 24,
+            }
+        }
+    }
+
+    impl Header {
+        pub fn new() -> Self {
+            Self::default()
+        }
+    }
+
+    impl Widget for Header {
+        fn build(&self) -> Html {
+            let selector = self.textarea_selector.clone();
+            let onclick = Callback::from(move |_: MouseEvent| replace_selected_in_textarea(&selector, ("### ", "")));
+            html! {
+                <button class = &self.class style = format!("width: {0}; height: {0}", self.size)
+                        type = "button" title = &self.title onclick = onclick>
+                    <svg xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 24 24" width = self.size height = self.size>
+                        <path fill-rule = "evenodd" d = "M6.25 4a.75.75 0 01.75.75V11h10V4.75a.75.75 0 011.5 0v14.5a.75.75 0 \
+                                01-1.5 0V12.5H7v6.75a.75.75 0 01-1.5 0V4.75A.75.75 0 016.25 4z">
+                        </path>
+                    </svg>
+                </button>
+            }
+        }
+    }
+
     pub struct Bold {
         pub textarea_selector: String,
         pub class: String,
@@ -142,7 +187,7 @@ pub mod tool {
         fn default() -> Self {
             Self {
                 textarea_selector: ".lew-simple__textarea".to_string(),
-                class: "lew-simple__tool_bold".to_string(),
+                class: "lew-simple__tool_button".to_string(),
                 title: "Bold".to_string(),
                 size: 24,
             }
@@ -166,6 +211,47 @@ pub mod tool {
                         <path fill-rule = "evenodd" d = "M6 4.75c0-.69.56-1.25 1.25-1.25h5a4.75 4.75 0 013.888 7.479A5 5 0 \
                                 0114 20.5H7.25c-.69 0-1.25-.56-1.25-1.25V4.75zM8.5 13v5H14a2.5 2.5 0 000-5H8.5zm0-2.5h3.751A2.25 \
                                 2.25 0 0012.25 6H8.5v4.5z">
+                        </path>
+                    </svg>
+                </button>
+            }
+        }
+    }
+
+    pub struct Italic {
+        pub textarea_selector: String,
+        pub class: String,
+        pub title: String,
+        pub size: u32,
+    }
+
+    impl Default for Italic {
+        fn default() -> Self {
+            Self {
+                textarea_selector: ".lew-simple__textarea".to_string(),
+                class: "lew-simple__tool_button".to_string(),
+                title: "Italic".to_string(),
+                size: 24,
+            }
+        }
+    }
+
+    impl Italic {
+        pub fn new() -> Self {
+            Self::default()
+        }
+    }
+
+    impl Widget for Italic {
+        fn build(&self) -> Html {
+            let selector = self.textarea_selector.clone();
+            let onclick = Callback::from(move |_: MouseEvent| replace_selected_in_textarea(&selector, "*"));
+            html! {
+                <button class = &self.class style = format!("width: {0}; height: {0}", self.size)
+                        type = "button" title = &self.title onclick = onclick>
+                    <svg xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 24 24" width = self.size height = self.size>
+                        <path fill-rule = "evenodd" d = "M10 4.75a.75.75 0 01.75-.75h8.5a.75.75 0 010 1.5h-3.514l-5.828 \
+                                13h3.342a.75.75 0 010 1.5h-8.5a.75.75 0 010-1.5h3.514l5.828-13H10.75a.75.75 0 01-.75-.75z">
                         </path>
                     </svg>
                 </button>

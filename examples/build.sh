abdc01918058e31fa1a75496664008c367313fd5
Alexander XX <freecoder.xx@gmail.com>
2020-06-19 00:15:58 +0300

    Added optimisation flags

--- examples/build.sh
+++ examples/build.sh
@@ -4,5 +4,5 @@ example=${1}
 mode=${2:+"--release"}
 out_dir=${2:-debug}
 
-cargo build --example $example --target wasm32-unknown-unknown $mode
+RUSTFLAGS="-Clto -Copt-level=s" cargo build --example $example --target wasm32-unknown-unknown $mode
 wasm-bindgen --target web --no-typescript --out-dir examples/static/target --out-name $example target/wasm32-unknown-unknown/${out_dir}/examples/${example}.wasm
